207d7df5e54589a779ffcd1e4d6829d3e44a7a00
Kateryna Oblakevych <malutina.catya14@gmail.com>
2022-11-20 23:20:30 +0200

    Initial commit.

---
+++ src/test/java/com/company/realtimerate/RealtimeRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.realtimerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
