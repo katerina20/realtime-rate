38e79a13856cdb5d68a674d330517c34ffaf7d80
Kateryna Oblakevych <malutina.catya14@gmail.com>
2022-11-22 22:01:00 +0200

    Add first immediate response after request.

--- src/main/java/com/company/realtimerate/controller/RateController.java
+++ src/main/java/com/company/realtimerate/controller/RateController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
@@ -19,7 +20,9 @@ public class RateController {
 
     @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<String> streamRate() {
-        return Flux.interval(Duration.ofSeconds(30))
-                   .map(sequence -> rateService.getRateString());
+        Mono<String> first = Mono.just(1).map(s -> rateService.getRateString());
+        Flux<String> rest = Flux.interval(Duration.ofSeconds(30))
+                                .map(sequence -> rateService.getRateString());
+        return Flux.concat(first, rest);
     }
 }
