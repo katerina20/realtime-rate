0011e5612a83c9d9b22e18886a3af6cedb46a50f
Kateryna Oblakevych <malutina.catya14@gmail.com>
2022-11-22 12:02:44 +0200

    Change string format.

--- src/main/java/com/company/realtimerate/service/RateService.java
+++ src/main/java/com/company/realtimerate/service/RateService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
@@ -14,14 +15,19 @@ import java.util.stream.DoubleStream;
 @Service
 public class RateService {
 
-    private static final String RATE_STRING_FORMAT = "Текущий курс: %s Средний курс: %s Дата: %s";
+    private static final String RATE_STRING_FORMAT = "Текущий курс: %.2f Средний курс: %.2f Дата: %s";
+
+    private static final String DATETIME_PATTERN = "dd.MM.yyyy HH:mm:ss";
 
     @Autowired
     RateRepository rateRepository;
 
     public String getRateString() {
         List<Rate> rates = rateRepository.findAll();
-        return String.format(RATE_STRING_FORMAT, getLastRate(rates), getAverageRate(rates), LocalDateTime.now());
+        return String.format(RATE_STRING_FORMAT,
+                getLastRate(rates),
+                getAverageRate(rates),
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern(DATETIME_PATTERN)));
     }
 
     private double getLastRate(List<Rate> rates) {
